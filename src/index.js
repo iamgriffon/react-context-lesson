217052d3add5479c6028c14687bfd1a6ac94a4ad
Gustavo Dupin <55361396+iamgriffon@users.noreply.github.com>
2020-07-15 13:25:47 -0300

    added react apollo and started setting graphql

--- src/index.js
+++ src/index.js
@@ -3,13 +3,49 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import  CartProvider from './providers/cart/cart-provider';
 import './index.css';
+import { ApolloProvider } from 'react-apollo';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloClient, gql } from 'apollo-boost';
+
 import App from './App';
 
+const httpLink = createHttpLink({
+  uri: 'https://crwn-clothing.com',
+  credentials: 'same-origin',
+});
+
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache
+});
+
+client.query({
+  query: gql`
+    {
+      getCollectionsByTitle(title: "hats") {
+        id
+        title
+        items{
+          name
+          id
+          price
+          imageUrl
+        }
+      }
+    }
+  `
+}).then(res => console.log(res));
+
 ReactDOM.render(
-  <CartProvider>
-      <BrowserRouter>
-         <App />
-      </BrowserRouter>
-  </CartProvider>,
+  <ApolloProvider client={client}>
+    <CartProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+    </CartProvider>
+  </ApolloProvider>,
   document.getElementById('root')
 );
